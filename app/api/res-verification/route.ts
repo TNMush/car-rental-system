13c983faa6011a5c40d5ecd33e962d717e515e83
tnmush <tnmush@yahoo.com>
2024-07-19 20:09:06 +0200

    residence interfaces

--- app/api/res-verification/route.ts
+++ app/api/res-verification/route.ts
@@ -1,6 +1,10 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { ResidenceVerificationSchema } from "./schema";
+import {
+  AdminResidenceVerificationInterface,
+  AdminResidenceVerificationSchema,
+  ResidenceVerificationSchema,
+} from "./schema";
 
 export async function POST(request: NextRequest) {
   try {
@@ -63,6 +67,69 @@ export async function POST(request: NextRequest) {
 
     // 5. Return the created or updated residence verification
     return NextResponse.json(newResidenceVerification, { status: 201 });
+  } catch (error) {
+    console.log("Unexpected error occurred", error);
+    return NextResponse.json(
+      { error: `Unexpected error occurred: ${error}` },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(request: NextRequest) {
+  try {
+    const requestBody: AdminResidenceVerificationInterface =
+      await request.json();
+
+    // 1. Validation
+    const validation = AdminResidenceVerificationSchema.safeParse(requestBody);
+    if (!validation.success) {
+      return NextResponse.json(
+        { message: validation.error.errors },
+        { status: 400 }
+      );
+    }
+
+    // 2. Check if profile exists
+    const profile = await prisma.profile.findFirst({
+      where: {
+        id: requestBody.id,
+      },
+    });
+    if (!profile) {
+      return NextResponse.json(
+        { message: "Profile doesn't exist" },
+        { status: 404 }
+      );
+    }
+
+    // 3. Check if residence verification already submitted
+    const existingVerification = await prisma.residenceVerification.findFirst({
+      where: {
+        id: requestBody.id,
+      },
+    });
+
+    if (!existingVerification) {
+      return NextResponse.json(
+        { message: "Residence verification doesn't exist" },
+        { status: 404 }
+      );
+    }
+
+    // 4. Update existing ResidenceVerification
+    const updatedResidenceVerification =
+      await prisma.residenceVerification.update({
+        where: {
+          id: requestBody.id,
+        },
+        data: {
+          status: requestBody.status,
+        },
+      });
+
+    // 5. Return the updated residence verification
+    return NextResponse.json(updatedResidenceVerification, { status: 200 });
   } catch (error) {
     console.log("Unexpected error occurred", error);
     return NextResponse.json(
--- app/api/res-verification/schema.ts
+++ app/api/res-verification/schema.ts
@@ -5,3 +5,13 @@ export const ResidenceVerificationSchema = z.object({
   utilityBill: z.string().url(),
   affidavity: z.string().optional(),
 });
+
+export const AdminResidenceVerificationSchema = z.object({
+  id: z.string(),
+  status: z.enum(["FAILED", "SUCCESS"]),
+});
+
+export type ResidenceVerification = z.infer<typeof ResidenceVerificationSchema>;
+export type AdminResidenceVerificationInterface = z.infer<
+  typeof AdminResidenceVerificationSchema
+>;
